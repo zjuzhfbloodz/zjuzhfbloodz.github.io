374bee68893c9774ac15f952b051b423357057bd
zjuzhfbloodz <2453183055@qq.com>
2026-02-05 14:56:12 +0800

    Site updated: 2026-02-05 14:56:11

--- pets/images-data.js
+++ pets/images-data.js
@@ -1,5 +1,5 @@
 // 自动生成 - 不要手动修改
-// 最后更新: 2026/02/05 14:52:26
+// 最后更新: 2026/02/05 14:56:11
 // 运行: node scripts/generate-pet-images.js 更新数据
 
 const petImagesData = {
@@ -11,6 +11,13 @@ const petImagesData = {
     ],
     "images": {
         "2026-02-05": [
+            {
+                "date": "2026-02-05",
+                "time": "14:56",
+                "img": "/images/pets/pet_20260205_145611.jpg",
+                "type": "pet",
+                "found": true
+            },
             {
                 "date": "2026-02-05",
                 "time": "14:52",
