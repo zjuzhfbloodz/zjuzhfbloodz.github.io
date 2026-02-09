f47d125fa9a21bea36af9ee68eed132458c0d157
zjuzhfbloodz <2453183055@qq.com>
2026-02-09 09:05:07 +0800

    Site updated: 2026-02-09 09:05:06

--- pets/images-data.js
+++ pets/images-data.js
@@ -1,5 +1,5 @@
 // 自动生成 - 不要手动修改
-// 最后更新: 2026/2/9 08:35:04
+// 最后更新: 2026/2/9 09:05:04
 // 运行: node scripts/generate-pet-images.js 更新数据
 
 const petImagesData = {
@@ -15,6 +15,13 @@ const petImagesData = {
 ],
     images: {
   "2026-02-09": [
+    {
+      "date": "2026-02-09",
+      "time": "09:01",
+      "img": "/images/pets/pet_20260209_090106.jpg",
+      "type": "pet",
+      "found": false
+    },
     {
       "date": "2026-02-09",
       "time": "08:31",
@@ -11673,10 +11680,10 @@ const petImagesData = {
   ]
 },
     stats: {
-        totalImages: 1663,
+        totalImages: 1664,
         totalDays: 8,
         foundCount: 31,
-        notFoundCount: 1632,
+        notFoundCount: 1633,
         dogCount: 24,
         catCount: 7
     },
