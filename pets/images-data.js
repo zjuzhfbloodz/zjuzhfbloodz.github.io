3cef6d17439c3ae1cfce2a14014ede7a3288400d
zjuzhfbloodz <2453183055@qq.com>
2026-02-16 11:31:35 +0800

    Site updated: 2026-02-16 11:31:34

--- pets/images-data.js
+++ pets/images-data.js
@@ -1,5 +1,5 @@
 // 自动生成 - 不要手动修改
-// 最后更新: 2026/2/16 11:05:05
+// 最后更新: 2026/2/16 11:31:33
 // 支持 AI JSON+summary 格式
 
 const petImagesData = {
@@ -22,6 +22,17 @@ const petImagesData = {
 ],
     images: {
   "2026-02-16": [
+    {
+      "date": "2026-02-16",
+      "time": "11:31",
+      "img": "/images/pets/pet_20260216_113111.jpg",
+      "type": "pet",
+      "found": false,
+      "petName": null,
+      "location": "",
+      "action": "",
+      "summary": "画面中未发现宠物 Mango、Nomi 或 小义乌。"
+    },
     {
       "date": "2026-02-16",
       "time": "11:01",
@@ -22185,10 +22196,10 @@ const petImagesData = {
   ]
 },
     stats: {
-        totalImages: 2012,
+        totalImages: 2013,
         totalDays: 15,
         foundCount: 45,
-        notFoundCount: 1967,
+        notFoundCount: 1968,
         dogCount: 33,
         catCount: 12
     },
