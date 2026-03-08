82cb25d21fc6280b0d3b3a0b87d98baadc315af8
zjuzhfbloodz <2453183055@qq.com>
2026-03-09 03:03:24 +0800

    Site updated: 2026-03-09 03:03:23

--- pets/images-data.js
+++ pets/images-data.js
@@ -27,6 +27,17 @@ const petImagesData = {
     "2026-03-09": [
       {
         "date": "2026-03-09",
+        "time": "03:00",
+        "img": "/images/pets/20260309-030000-BD0673744.jpg",
+        "type": "other",
+        "boxedImg": null,
+        "analysis": "image视觉识别和YOLO目标检测均未检测到人/猫/狗。图片模糊失焦，无法识别具体内容。",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "both"
+      },
+      {
+        "date": "2026-03-09",
         "time": "02:30",
         "img": "/images/pets/20260309-023000-BD0673744.jpg",
         "type": "other",
