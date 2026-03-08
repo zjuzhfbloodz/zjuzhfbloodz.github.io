1b5dfd589be92aa32067914c290d2c032e99dd9a
zjuzhfbloodz <2453183055@qq.com>
2026-03-08 21:11:52 +0800

    Site updated: 2026-03-08 21:11:51

--- pets/images-data.js
+++ pets/images-data.js
@@ -27,6 +27,17 @@ const petImagesData = {
     "2026-03-08": [
       {
         "date": "2026-03-08",
+        "time": "21:00",
+        "img": "/images/pets/20260308-210000-BD0673744.jpg",
+        "type": "other",
+        "boxedImg": null,
+        "analysis": "image视觉识别和YOLO目标检测均未检测到人/猫/狗。图片模糊，整体呈灰色调，无明显特征。",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "both"
+      },
+      {
+        "date": "2026-03-08",
         "time": "20:30",
         "img": "/images/pets/20260308-203000-BD0673744.jpg",
         "type": "other",
