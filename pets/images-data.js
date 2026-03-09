fdf17c0ea82d23258fc9913e5e4eef8c8dc505f0
zjuzhfbloodz <2453183055@qq.com>
2026-03-10 02:03:24 +0800

    Site updated: 2026-03-10 02:03:23

--- pets/images-data.js
+++ pets/images-data.js
@@ -15044,6 +15044,18 @@ const petImagesData = {
     "2026-03-10": [
       {
         "date": "2026-03-10",
+        "time": "02:00",
+        "img": "/images/pets/20260310-020000-BD0673744.jpg",
+        "type": "other",
+        "found": false,
+        "boxedImg": null,
+        "analysis": "图片模糊，image 工具和 YOLO 均未识别到人/猫/狗。",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "both"
+      },
+      {
+        "date": "2026-03-10",
         "time": "00:30",
         "img": "/images/pets/20260310-003046-BD0673744.jpg",
         "type": "other",
