59e2f701ea6f12091d0081ac9405a762fe1c3131
zjuzhfbloodz <2453183055@qq.com>
2026-03-10 13:02:57 +0800

    Site updated: 2026-03-10 13:02:56

--- pets/images-data.js
+++ pets/images-data.js
@@ -65,6 +65,18 @@ const petImagesData = {
     "2026-03-10": [
       {
         "date": "2026-03-10",
+        "time": "13:00",
+        "img": "/images/pets/20260310-130000-BD0673744.jpg",
+        "type": "other",
+        "found": false,
+        "boxedImg": "/images/pets/annotated/20260310-130000-BD0673744.jpg",
+        "analysis": "图片模糊，image 工具和 YOLO 均未识别到人/猫/狗。",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "both"
+      },
+      {
+        "date": "2026-03-10",
         "time": "12:30",
         "img": "/images/pets/20260310-123000-BD0673744.jpg",
         "type": "other",
@@ -15213,7 +15225,7 @@ const petImagesData = {
     ]
   },
   "stats": {
-    "totalImages": 2123,
+    "totalImages": 2124,
     "personCount": 51,
     "catCount": 71,
     "dogCount": 73
