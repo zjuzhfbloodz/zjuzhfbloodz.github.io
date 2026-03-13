6786b7fcf2c10456eff81b0f824b9529695b6817
zjuzhfbloodz <2453183055@qq.com>
2026-03-13 09:33:00 +0800

    Site updated: 2026-03-13 09:32:59

--- pets/images-data.js
+++ pets/images-data.js
@@ -15504,6 +15504,18 @@ const petImagesData = {
     "2026-03-13": [
       {
         "date": "2026-03-13",
+        "time": "09:30",
+        "img": "/images/pets/20260313-093000-BD0673744.jpg",
+        "type": "other",
+        "found": false,
+        "boxedImg": null,
+        "analysis": "image工具因API地区限制不可用。YOLO检测：无检测结果（conf=0.25, classes=0,15,16）。最终判定为other。",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "yolo"
+      },
+      {
+        "date": "2026-03-13",
         "time": "08:30",
         "img": "/images/pets/20260313-083024-BD0673744.jpg",
         "type": "other",
@@ -15628,5 +15640,4 @@ const petImagesData = {
   "catCount": 71,
   "dogCount": 73
 };
-
 export default petImagesData;
