cf85412ab25be455c5c76ee22cff34cc14dcf943
zjuzhfbloodz <2453183055@qq.com>
2026-03-22 06:34:49 +0800

    Site updated: 2026-03-22 06:34:47

--- pets/images-data.js
+++ pets/images-data.js
@@ -28,6 +28,18 @@ const petImagesData = {
     "2026-03-22": [
       {
         "date": "2026-03-22",
+        "time": "06:30",
+        "img": "/images/pets/20260322-063000-BD0673744.jpg",
+        "type": "other",
+        "found": false,
+        "boxedImg": "/images/pets/annotated/20260322-063000-BD0673744.jpg",
+        "analysis": "image: 图像模糊，未识别到人/猫/狗；YOLO: 无检测",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "both"
+      },
+      {
+        "date": "2026-03-22",
         "time": "06:00",
         "img": "/images/pets/20260322-060000-BD0673744.jpg",
         "type": "other",
@@ -15262,7 +15274,7 @@ const petImagesData = {
     ]
   },
   "stats": {
-    "totalImages": 2127,
+    "totalImages": 2128,
     "personCount": 51,
     "catCount": 72,
     "dogCount": 73
