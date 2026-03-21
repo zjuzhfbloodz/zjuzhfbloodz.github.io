825042e400266dce974071f5cc866ddfda224571
zjuzhfbloodz <2453183055@qq.com>
2026-03-21 20:35:47 +0800

    Site updated: 2026-03-21 20:35:46

--- pets/images-data.js
+++ pets/images-data.js
@@ -27,6 +27,18 @@ const petImagesData = {
     "2026-03-21": [
       {
         "date": "2026-03-21",
+        "time": "20:30",
+        "img": "/images/pets/20260321-203000-BD0673744.jpg",
+        "type": "other",
+        "found": false,
+        "boxedImg": null,
+        "analysis": "image: API连接失败；YOLO:无检测(person/cat/dog)",
+        "petIdentity": null,
+        "yoloClasses": [],
+        "decisionSource": "yolo"
+      },
+      {
+        "date": "2026-03-21",
         "time": "20:00",
         "img": "/images/pets/20260321-200000-BD0673744.jpg",
         "type": "other",
@@ -15079,7 +15091,7 @@ const petImagesData = {
     ]
   },
   "stats": {
-    "totalImages": 2112,
+    "totalImages": 2113,
     "personCount": 51,
     "catCount": 71,
     "dogCount": 73
