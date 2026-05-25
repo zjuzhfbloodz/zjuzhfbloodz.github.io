41a1fd9560d586667c3cc4cb3c38ace42b66899e
zjuzhfbloodz <2453183055@qq.com>
2026-05-25 23:55:49 +0800

    Site updated: 2026-05-25 23:55:47

--- pets/diaries-data.js
+++ pets/diaries-data.js
@@ -5,9 +5,10 @@ const petDiaryData = {
       "title": "2026-05-25 宠物生活日记",
       "url": "/post/pets-lifelog-2026-05-25.html",
       "cover": "/images/pets-lifelog/2026-05-25/003509.jpg",
-      "summary": "记录到 ywu 的居家片段，共 3 次画面。",
-      "eventCount": 3,
+      "summary": "记录到 moni、ywu 的居家片段，共 4 次画面。",
+      "eventCount": 4,
       "pets": [
+        "moni",
         "ywu"
       ]
     }
